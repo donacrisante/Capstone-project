e4211145f694f5c5fcdc9268c5b2964e1b6ce437
Donatella Crisante <dona.crisante@zoho.com>
2023-08-15 21:25:31 +0200

    rename list page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import GlobalStyle from "@/styles";
 import Head from "next/head";
 import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
-import JourneyList from "./journeyList";
 
 export default function App({ Component, pageProps }) {
   const [entries, setEntries] = useLocalStorageState("entries", {
@@ -31,11 +30,6 @@ export default function App({ Component, pageProps }) {
         <title>Capstone Project</title>
       </Head>
       <Component {...pageProps} handleFormSubmit={handleFormSubmit} />
-      <JourneyList
-        entries={entries}
-        filter={filter}
-        handleShowAllEntries={handleShowAllEntries}
-      />
     </>
   );
 }
