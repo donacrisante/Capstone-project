7227bbadab2baa6cb6d5ecfdb1ac7a27b4374cb4
Donatella Crisante <dona.crisante@zoho.com>
2023-08-15 22:44:11 +0200

    access to localstorage on journeylist page

--- pages/journeyList/index.js
+++ pages/journeyList/index.js
@@ -1,6 +1,13 @@
 //import EntryList from "@/components/EntryList";
+// import ls from 'local-storage'
 
 export default function JourneyList(){
+  if (typeof window !== 'undefined') {
+    // Perform localStorage action
+    const entries = localStorage.getItem("entries");
+  console.log("entries: ", entries);
+  }
+  
   return <div>journeyList hier</div>
 }
 // export default function JourneyList({ entries, filter, onShowAllEntries }) {
