909579a5d8fd7a2f77798e1ac922427c8836a611
Donatella Crisante <dona.crisante@zoho.com>
2023-08-23 12:54:31 +0200

    add edit button and edit page

--- components/EntryList.js
+++ components/EntryList.js
@@ -5,6 +5,7 @@ import Tabs from "./Tabs";
 import Badge from "./Badge";
 import FavouriteButton from "./FavouriteButton";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function EntryList({
   entries,
@@ -15,6 +16,8 @@ export default function EntryList({
   favouriteEntriesCount,
   onToggleFavourite,
 }) {
+  const [selectedEntry, setSelectedEntry] = useState(null); // State to track the selected entry for editing
+  const [updatedEntry, setUpdatedEntry] = useState(null); // State to store the updated entry data
 
   const router = useRouter();
 
@@ -22,40 +25,68 @@ export default function EntryList({
     router.back();
   }
 
+  function handleEditEntry(entry) {
+    // Set the selected entry for editing
+    setSelectedEntry(entry);
+    setUpdatedEntry({ ...entry }); // Initialize the form with the current data
+    router.push("/editForm");
+  }
+
+
   return (
     <>
       <h2>Journeys</h2>
       <section name="journeys-section">
-      <Tabs>
-        <Tab onClick={onShowAllEntries} isActive={filter === "all"}>
-          Recent journeys{" "}
-          <Badge isActive={filter === "all"}>{allEntriesCount}</Badge>
-        </Tab>
-        <Tab
-          onClick={onShowFavouriteEntries}
-          isActive={filter === "favourites"}
-        >
-          Favourites{" "}
-          <Badge isActive={filter === "favourites"}>
-            {favouriteEntriesCount}
-          </Badge>
-        </Tab>
-      </Tabs>
-      <div name="journey-section__entries">
-        {entries.map((entry, index) => (
-          <Fragment key={entry.id}>
-            {index > 0 ? <Divider /> : null}
-            <FavouriteButton
-              id={entry.id}
-              isFavourite={entry.isFavourite}
-              onToggleFavourite={onToggleFavourite}
-            />
-            <p>{entry.date}, {entry.start} - {entry.destination}, {entry.km}km, {entry.transport} {entry.fuel}, {entry.result} kg CO<sub>2</sub></p>
-          </Fragment>
-        ))}
-      </div>
+        <Tabs>
+          <Tab onClick={onShowAllEntries} isActive={filter === "all"}>
+            Recent journeys{" "}
+            <Badge isActive={filter === "all"}>{allEntriesCount}</Badge>
+          </Tab>
+          <Tab
+            onClick={onShowFavouriteEntries}
+            isActive={filter === "favourites"}
+          >
+            Favourites{" "}
+            <Badge isActive={filter === "favourites"}>
+              {favouriteEntriesCount}
+            </Badge>
+          </Tab>
+        </Tabs>
+        <div name="journey-section__entries">
+          {entries.map((entry, index) => (
+            <Fragment key={entry.id}>
+              {index > 0 ? <Divider /> : null}
+              <FavouriteButton
+                id={entry.id}
+                isFavourite={entry.isFavourite}
+                onToggleFavourite={onToggleFavourite}
+              />
+              <p>
+                {entry.date}, {entry.start} - {entry.destination}, {entry.km}km,{" "}
+                {entry.transport} {entry.fuel}, {entry.result} kg CO<sub>2</sub>
+              </p>
+              <button onClick={() => handleEditEntry(entry)}>Edit</button>
+            </Fragment>
+          ))}
+        </div>
       </section>
-      <button type="submit" onClick={handleBackToForm}> + </button>
+      {/* {selectedEntry && (
+        <div>
+          <h3>Edit Entry</h3>
+          {/* Replace this with your edit form fields */}
+          {/* <input
+            type="text"
+            value={updatedEntry.start}
+            onChange={(event) => setUpdatedEntry({ ...updatedEntry, start: event.target.value })}
+          /> */}
+          {/* Add more input fields for other entry properties */}
+        {/*   <button onClick={handleUpdateEntry}>Update</button>
+        </div>
+      )}  */}
+      <button type="submit" onClick={handleBackToForm}>
+        {" "}
+        +{" "}
+      </button>
     </>
   );
 }
