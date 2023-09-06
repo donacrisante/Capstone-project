ee6ba1778353c95114dc978a3ea0214a8546b058
Donatella Crisante <dona.crisante@zoho.com>
2023-09-06 09:58:48 +0200

    delete useless file and rename overview page into chart page

--- pages/chart/index.js
+++ pages/chart/index.js
@@ -1,105 +1,166 @@
+import styled from "styled-components";
 import { Chart as ChartJS } from "chart.js/auto";
-import { useRouter } from "next/router";
 import { Bar } from "react-chartjs-2";
 
-    const transportTypes = [
-      "Car petrol",
-      "Car diesel",
-      "Car hybrid",
-      "Car Electric-Strommix",
-      "Car Electric-Renewable",
-      "Plane",
-      "Train",
-      "Bicycle",
-    ];
-    const monthLabels = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
+export default function Overview({ entries }) {
+  const totalEmissionsPerTransport = (transport, fuel, month) =>
+    Math.floor(
+      entries
+        .filter(
+          (entry) =>
+            entry.transport === transport &&
+            entry.fuel === fuel /* &&
+            new Date(entry.date).getMonth() === month */
+        )
+        .reduce((acc, curr) => acc + curr.result, 0)
+    );
 
-    function getBackgroundColor(index) {
-    // Define a function to generate unique colors for each transport type
-    const colors = [
-      "rgba(255, 99, 132, 0.7)",
-      "rgba(54, 162, 235, 0.7)",
-      "rgba(255, 206, 86, 0.7)",
-      "rgba(75, 192, 192, 0.7)",
-      "rgba(5, 112, 112, 0.7)",
-      "rgba(3, 32, 158, 0.7)",
-      "rgba(204, 94, 163, 0.7)",
-      "rgba(204, 167, 94, 0.7)",
-    ];
-    return colors[index % colors.length];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  /* const transportData = [
+    "car petrol",
+    "car diesel",
+    "car hybrid",
+    "car electric-strommix",
+    "car electric-renewable",
+    "train",
+    "plane",
+    "bicycle",
+  ];
+
+  const monthlyData = months.map((month, monthIndex) => ({
+    month,
+    data: transportData.map((transport) =>
+      totalEmissionsPerTransport(transport, monthIndex)
+    ),
+  })); */
+
+  const carPetrol = totalEmissionsPerTransport("car", "petrol");
+  const carDiesel = totalEmissionsPerTransport("car", "diesel");
+  const carHybrid = totalEmissionsPerTransport("car", "hybrid");
+  const carElectricStrommix = totalEmissionsPerTransport(
+    "car",
+    "electric-strommix"
+  );
+  const carElectricRenewable = totalEmissionsPerTransport(
+    "car",
+    "electric-renewable"
+  );
+  const train = totalEmissionsPerTransport("train");
+  const plane = totalEmissionsPerTransport("plane");
+  const bicycle = totalEmissionsPerTransport("bicycle");
+
+  /* const data = {
+    labels: months,
+    datasets: transportData.map((transport, index) => ({
+      label: transport,
+      data: monthlyData.map((transport) => transport.data[index]),
+      backgroundColor: [
+        "#FF5733", // Red
+        "#33FF57", // Green
+        "#5733FF", // Blue
+        "#FF33B8", // Pink
+        "#33FFB8", // Turquoise
+        "#FFC933", // Yellow
+        "#33C9FF", // Light Blue
+        "#FF3385", // Coral
+      ]
+    })),
+  }; */
+
+    const data = {
+    labels: months,
+    datasets: [
+      {
+        label: "Car petrol",
+        data: [carPetrol],
+        backgroundColor: "violet",
+      },
+      {
+        label: "Car diesel",
+        data: [carDiesel],
+        backgroundColor: "orange",
+      },
+      {
+        label: "Car hybrid",
+        data: [carHybrid],
+        backgroundColor: "yellow",
+      },
+      {
+        label: "Car Electric-Strommix",
+        data: [carElectricStrommix],
+        backgroundColor: "pink",
+      },
+      {
+        label: "Car Electric-Renewable",
+        data: [carElectricRenewable],
+        backgroundColor: "turquoise",
+      },
+      {
+        label: "Train",
+        data: [train],
+        backgroundColor: "lightgreen",
+      },
+      {
+        label: "Plane",
+        data: [plane],
+        backgroundColor: "coral",
+      },
+      {
+        label: "Bicycle",
+        data: [bicycle],
+        backgroundColor: "lightblue",
+      },
+    ],
   };
-
-    const emissionsData = {
-      labels: monthLabels,
-      datasets: transportTypes.map((transport, index) => ({
-        label: transport,
-        data: monthLabels.map((month) => {
-          // Calculate total CO2 emissions for the specific transport and month
-          const totalEmissions = entries
-            .filter(
-              (entry) =>
-                entry.transport === transport && entry.date.includes(month)
-            )
-            .reduce((acc, entry) => acc + entry.result, 0);
-
-          return totalEmissions;
-        }),
-        backgroundColor: getBackgroundColor(index), // Define a function to assign unique colors
-      })),
-    };
-
-    // Create the chart
-    /* const ctx = chartRef.current.getContext("2d"); */
-    const config = {
-      type: "bar",
-      data: emissionsData,
-      options: {
-        plugins: {
-          title: {
-            display: true,
-            text: "CO2 Emissions per Means of Transport",
-          },
-        },
-        responsive: true,
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-          },
+ 
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "CO2 Emissions per Means of Transport",
         },
       },
-    };
-   [entries];
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    },
+  };
 
-
-  export default function OverviewChart({entries}) {
-    const router = useRouter();
-     /* const chartRef = useRef(null); */
   return (
     <>
-      <h2>CO2 Emissions per Means of Transport</h2>
-      <div style={{ width: 700 }}>
-      <Bar data={emissionsData} config={config} entries={entries}/>
-    </div>
-      {/* <canvas ref={chartRef}></canvas> */}
-      <button onClick={() => router.push("/journey-list")}>
-        Back to Journey List
-      </button>
+      <Chart>
+        <Bar data={data} config={config} />
+      </Chart>
+      <button type="button">Weekly</button>
+      <button type="button">Monthly</button>
+      <button type="button">Yearly</button>
     </>
   );
 }
+
+const Chart = styled.div`
+  width: 700px;
+`;
